7bb33b62b6fc9cf752402f7d1d035b90dddf8c7a
Petardan <danilovski.petar@gmail.com>
2017-09-29 23:13:11 +0200

    Added complete large repairman info fragment

--- app/src/main/java/com/rpd/irepair/LargeRepairmanInfoFragment.java
+++ app/src/main/java/com/rpd/irepair/LargeRepairmanInfoFragment.java
@@ -2,9 +2,12 @@ package com.rpd.irepair;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 /**
@@ -13,30 +16,77 @@ import android.widget.TextView;
 
 public class LargeRepairmanInfoFragment extends DialogFragment {
 
+    Repairman repairman;
+
     TextView repairmanNameAndSurname;
 
+    TextView largeFragmentDescriptionView;
+    TextView largeFragmentProfessionsView;
+    TextView largeFragmentRegionsView;
+    TextView largeFragmentAddressView;
+    TextView largeFragmentEmailView;
+    TextView largeFragmentMobile1View;
+    TextView largeFragmentMobile2View;
+
+    Button acceptRepairmanButton;
+    Button declineRepairmanButton;
+
+    RatingBar ratingBar;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.large_repairman_fragment, container, false);
 
         repairmanNameAndSurname = (TextView)rootView.findViewById(R.id.largeFragmentNameAndSurname);
+
+        largeFragmentDescriptionView = (TextView)rootView.findViewById(R.id.largeFragmentDescriptionView);
+        largeFragmentProfessionsView = (TextView)rootView.findViewById(R.id.largeFragmentProfessionsView);
+        largeFragmentRegionsView = (TextView)rootView.findViewById(R.id.largeFragmentRegionsView);
+        largeFragmentAddressView = (TextView)rootView.findViewById(R.id.largeFragmentAddressView);
+        largeFragmentEmailView = (TextView)rootView.findViewById(R.id.largeFragmentEmailView);
+        largeFragmentMobile1View = (TextView)rootView.findViewById(R.id.largeFragmentMobile1View);
+        largeFragmentMobile2View = (TextView)rootView.findViewById(R.id.largeFragmentMobile2View);
+
+        acceptRepairmanButton = (Button)rootView.findViewById(R.id.acceptRepairmanButton);
+        declineRepairmanButton = (Button)rootView.findViewById(R.id.declineRepairmanButton);
+
+        ratingBar = (RatingBar)rootView.findViewById(R.id.ratingBar);
+
         getDialog().setTitle("Title");
 
-        Repairman repairman = (Repairman) getArguments().getSerializable("REPAIRMAN");
+        repairman = (Repairman) getArguments().getSerializable("REPAIRMAN");
         repairmanNameAndSurname.setText(repairman.getFirstName() + " " + repairman.getLastName());
 
+        largeFragmentDescriptionView.setText(repairman.getDescription());
+        largeFragmentProfessionsView.setText(repairman.getProfessionsString());
+        largeFragmentRegionsView.setText(repairman.getRegionsString());
+        largeFragmentAddressView.setText(repairman.getAddress());
+        largeFragmentEmailView.setText(repairman.getEmail());
+        largeFragmentMobile1View.setText(repairman.getMobilePhone1());
+        largeFragmentMobile2View.setText(repairman.getMobilePhone2());
 
 
-        //Dismiss alarm fragment
-        /*stopAlarm.setOnClickListener(new View.OnClickListener() {
+        //Accept button onClick listener
+        acceptRepairmanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rootView, "Repairman accepted!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Decline button onClick listener
+        declineRepairmanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
             }
-        });*/
+        });
 
-
+        //Rating bar number of filled stars
+        ratingBar.setRating(Float.parseFloat(Double.valueOf(repairman.getAverageRating()).toString()));
 
         return rootView;
     }
