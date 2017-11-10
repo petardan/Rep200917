6cff9e797c8b7f3e9cc261d7c235b2d0bf8456c2
Petardan <danilovski.petar@gmail.com>
2017-11-10 22:03:02 +0100

    Starting adding fragments with on click titlefragment

--- app/src/main/java/com/rpd/AddJobFragments/AddJobTitleFragment.java
+++ app/src/main/java/com/rpd/AddJobFragments/AddJobTitleFragment.java
@@ -30,7 +30,6 @@ public class AddJobTitleFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
     }
 
     @Nullable
